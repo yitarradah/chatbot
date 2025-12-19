9668e9ba0719345fc1d78f0e1bba809ac32d80d0
yitarradah <yitarradah@gmail.com>
2025-12-19 12:24:45 +0300

    Fix: Bypass TSC build blocker and fix JSX config

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
     'process.env': process.env
   },
+  server: {
+    port: 3000
+  },
   build: {
     outDir: 'dist',
-    sourcemap: true
+    emptyOutDir: true
   }
 });
